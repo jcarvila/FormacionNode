7ca34effec56c61c5026bf89e9dc80ab7cf7ffe0
Juan Carlos Avila <juancarlos.avila@alestis.aero>
2020-09-16 10:18:46 +0200

    Primer commit: subimos a GitHub - master

---
+++ main.js
@@ -0,0 +1,27 @@
+function openBloque(evt, nombreBloque) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(nombreBloque).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  
+  document.getElementById("defaultOpen").click();
+
+  var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
